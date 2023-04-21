02f1350d4ebec4e35abe795e8f14492aa1697363
Raj Dave <10864@bpsdoha.edu.qa>
2023-04-21 10:37:40 +0300

    minor fixes

--- backend/MAT/helpers.py
+++ backend/MAT/helpers.py
@@ -1,7 +1,12 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-BASIC_STOP_WORDS = set(nltk.corpus.stopwords.words('english'))
+try:
+    BASIC_STOP_WORDS = set(nltk.corpus.stopwords.words('english'))
+except LookupError:
+    nltk.download('stopwords')
+    BASIC_STOP_WORDS = set(nltk.corpus.stopwords.words('english'))
+
 # with open('./data/MAT/stopwords.txt') as f:
 #     ADVANCED_STOP_WORDS = set(f.read().splitlines())
 
@@ -10,7 +15,7 @@ WNL = WordNetLemmatizer()
 
 def remove_stopwords(text: str, stopwords=None) -> list[str]:
     """Given a returns a list of tokens with stopwords removed"""
-    if stopwords == None:
+    if stopwords is None:
         stopwords = BASIC_STOP_WORDS
 
     tokens = WNL.tokenize(text)
@@ -18,5 +23,5 @@ def remove_stopwords(text: str, stopwords=None) -> list[str]:
 
 
 def remove_non_alpha(text: str) -> list[str]:
-    """Given a returns a list of tokens with non alphabetic characters removed"""
+    """Given a returns a list of tokens with non-alphabetic characters removed"""
     return [token.lower() for token in text.split(" ") if token.isalpha()]
