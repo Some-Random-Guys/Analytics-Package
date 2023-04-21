64ed86764eb9b632df0405bf9aebd0d0d923a04b
Raj Dave <10864@bpsdoha.edu.qa>
2023-04-21 10:37:27 +0300

    major logger changes

--- backend/logging_.py
+++ backend/logging_.py
@@ -4,33 +4,38 @@ import datetime
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from colorlog import ColoredFormatter
+from pydantic import BaseModel
+from logging.config import dictConfig
 
 
-def colorlogger(name: str = 'my-discord-bot') -> logging.Logger:
-    # Initialize the logger
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+class LogConfig(BaseModel):
+    """Logging configuration to be set for the server"""
 
-    # Define the log file path
-    log_dir = './logs'
-    os.makedirs(log_dir, exist_ok=True)
-    today = datetime.datetime.today().strftime('%Y-%m-%d')
-    log_file = os.path.join(log_dir, f'{today}.log')
+    LOGGER_NAME: str = "srg-api"
+    LOG_FORMAT: str = "%(levelprefix)s %(message)s"
+    LOG_LEVEL: str = "INFO"
 
-    # Add a console handler
-    console_handler = logging.StreamHandler()
-    console_formatter = ColoredFormatter(
-        "%(reset)s%(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s")
-    console_handler.setFormatter(console_formatter)
-    logger.addHandler(console_handler)
+    # Logging config
+    version = 1
+    disable_existing_loggers = False
+    formatters = {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": LOG_FORMAT,
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    }
+    handlers = {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    }
+    loggers = {
+        "srg-api": {"handlers": ["default"], "level": LOG_LEVEL},
+    }
 
-    # Add a file handler
-    file_handler = TimedRotatingFileHandler(log_file, when='midnight')
-    file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    file_handler.setFormatter(file_formatter)
-    logger.addHandler(file_handler)
 
-    return logger
-
-
-log = colorlogger()
+dictConfig(LogConfig().dict())
+log = logging.getLogger("srg-api")
