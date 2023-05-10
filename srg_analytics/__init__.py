2ac5554ecefde499ab832706b51470dda8519cd8
Raj Dave <10864@bpsdoha.edu.qa>
2023-05-10 18:14:58 +0300

    have one wordcloud function, add basic top_members function

---
+++ srg_analytics/__init__.py
@@ -0,0 +1,4 @@
+from .DB import DB
+from .etc import wordcloud
+from .helpers import most_used_words
+from schemas import DbCreds, DataTemplate
--- srg_analytics/etc.py
+++ srg_analytics/etc.py
@@ -1,6 +1,4 @@
-import io
 import random
-
 from wordcloud import WordCloud as wc
 import matplotlib.pyplot as plt
 import mplcyberpunk
@@ -8,7 +6,7 @@ from helpers import most_used_words
 from DB import DB
 
 
-async def wordcloud_user(db: DB, guild_id: int, user_id: int):
+async def wordcloud(db: DB, guild_id: int, user_id: int = None):
     messages: dict = await most_used_words(db=db, guild_id=guild_id, user_id=user_id or None, n=100)
 
     wordcloud = wc().generate_from_frequencies(messages)
@@ -28,21 +26,18 @@ async def wordcloud_user(db: DB, guild_id: int, user_id: int):
     return name
 
 
-async def wordcloud_server(guild_id):
-    messages: dict = {}  # todo: get messages from database
+async def top_members(db: DB, guild_id: int, n: int = 5, type_: str = "messages"):
+    if type_ == "messages":
+        await db.cur.execute(
+            f"""
+                SELECT author_id, COUNT(author_id) AS count
+                FROM `{guild_id}`
+                GROUP BY author_id
+                ORDER BY count DESC
+                LIMIT {n};
+            """
+        )
+        return db.cur.fetchall()
 
-    wordcloud = wc().generate_from_frequencies(messages)
-
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-
-    mplcyberpunk.add_glow_effects()
-
-    # save image
-    name = f"{random.randint(1, 100000000)}.png"
-    try:
-        plt.savefig(name, format='png')
-    except Exception as e:
-        print(e)
-
-    return name
+    if type_ == "words":
+        pass #todo
