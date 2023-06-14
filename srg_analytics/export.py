e2be78586370b3d6da29a448f7c8f3d1cb46553d
Raj Dave <10864@bpsdoha.edu.qa>
2023-06-14 21:14:35 +0300

    minor improvement to export_html func

--- srg_analytics/export.py
+++ srg_analytics/export.py
@@ -3,10 +3,7 @@ import chat_exporter
 
 
 async def export_html(client, channel, limit: int = None) -> io.BytesIO:
-    if limit is None:
-        res = await chat_exporter.export(channel, bot=client)
-    else:
-        res = await chat_exporter.export(channel, limit=limit, bot=client)
+    res = await chat_exporter.export(channel, bot=client, limit=limit)
 
     # return virtual file without using discord File class
     return io.BytesIO(res.encode())
