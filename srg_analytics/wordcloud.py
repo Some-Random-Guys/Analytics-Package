98f9bfe4116f8b01c96b51e750736f845c258065
Raj Dave <10864@bpsdoha.edu.qa>
2023-07-19 18:32:34 +0300

    [wordcloud] make the wordcloud bigger in size

--- srg_analytics/wordcloud.py
+++ srg_analytics/wordcloud.py
@@ -11,28 +11,28 @@ async def wordcloud(db: DB, guild_id: int, user_id: int = None, channel_id: int 
     top_words = await get_top_words(db=db, guild_id=guild_id, user_id=user_id, channel_id=channel_id, amount=100)
     # top_words = [(word, count), (word, count), ...]
 
+    dpi = 300
+
     # create a wordcloud
     try:
-        wordcloud = wc().generate_from_frequencies(dict(top_words))
+        wordcloud = wc(width=1920, height=1080).generate_from_frequencies(dict(top_words))
     except ValueError:
         return None
     # apply glow effects
     mplcyberpunk.add_glow_effects()
 
-    # Set DPI for higher resolution
-    dpi = 300
-
     # plot the wordcloud
-    plt.figure(figsize=(3840/dpi, 2160/dpi), dpi=dpi)
+    plt.figure(figsize=(20, 10), dpi=dpi)
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
 
     # save image    # todo fix res still being low
     name = f"{random.randint(1, 100000000)}.png"
     try:
-        plt.savefig(name, format='png', dpi=dpi)
+        plt.savefig(name, format='png', dpi=dpi, facecolor='k', bbox_inches='tight')
     except Exception as e:
         print(e)
+        return None
 
     plt.close()
 
