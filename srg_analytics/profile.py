cc27531312233e7c8ffa6ab1f925480832922094
Raj Dave <86198810+Rajdave69@users.noreply.github.com>
2024-03-16 13:55:16 +0530

    [profile] change method of calculating avg message count
    and many more smaller improvements

--- srg_analytics/profile.py
+++ srg_analytics/profile.py
@@ -31,9 +31,7 @@ async def top_words(db_or_msgs: DB | list[str], guild_id: int, user_id: int = No
     all_messages = all_messages.lower()
 
     words = nltk.word_tokenize(all_messages)
-
     words = (word for word in words if word.isalpha() and word not in stopwords)
-
     word_counts = Counter(words)
 
     return dict(word_counts.most_common(amount))
@@ -53,7 +51,7 @@ async def top_emoji(db_or_msgs: DB | list[str], guild_id: int, user_id: int = No
     emoji_counts = list(Counter(all_messages.split()))
 
     emojis = []
-    for block in emoji_counts:
+    for block in emoji_counts:    # TODO fix this, doesnt work as intended
         if emoji.is_emoji(block[0]):
             emojis.append(block)
 
@@ -117,30 +115,41 @@ async def get_character_count(db_or_msgs: DB | list[str], guild_id: int, user_id
 
 async def get_total_attachments(db: DB, guild_id: int, user_id: int) -> int:
     """Returns the total number of attachments in a guild."""
-    return (await db.execute(
-        # select sum of all values in sum_attachments column
-        f"SELECT SUM(num_attachments) FROM `{guild_id}` WHERE author_id = ?",
-        (str(user_id),),
-        fetch="one")
-            )[0]
+    return (
+        await db.execute(
+            # select sum of all values in sum_attachments column
+            f"SELECT SUM(num_attachments) FROM `{guild_id}` WHERE author_id = ?",
+            (str(user_id),), fetch="one"
+        )
+    )[0]
 
 
 async def get_total_embeds(db: DB, guild_id: int, user_id: int) -> int:
-    return (await db.execute(
-        f"SELECT COUNT(has_embed) FROM `{guild_id}` WHERE author_id = ?",
-        (str(user_id),),
-        fetch="one")
-            )[0]
+    return (
+        await db.execute(
+            f"SELECT COUNT(has_embed) FROM `{guild_id}` WHERE author_id = ?",
+            (str(user_id),), fetch="one"
+        )
+    )[0]
 
 
 async def is_bot(db: DB, guild_id: int, user_id: int) -> bool:
     """Returns whether a user is a bot or not."""
-    return (await db.execute(
-        f"SELECT is_bot FROM `{guild_id}` WHERE author_id = ? LIMIT 1",
-        (str(user_id),),
-        fetch="one")
-            )[0]
+    return (
+        await db.execute(
+            f"SELECT is_bot FROM `{guild_id}` WHERE author_id = ? LIMIT 1", 
+            (str(user_id),), fetch="one"
+        )
+    )[0]
 
+async def get_notnull_message_count(db: DB, guild_id: int, user_id: int):
+    return (
+        await db.execute(
+            f"SELECT count(*) FROM `{guild_id}` WHERE author_id = ? AND message_content IS NOT NULL AND message_content != ''",
+            (str(user_id),), fetch="one"
+        )
+    )[0]
+    
 
 async def build_profile(db: DB, guild_id: int, user_id: int) -> Profile:
     """Builds the profile for a certain user, in a certain guild."""
@@ -157,14 +166,14 @@ async def build_profile(db: DB, guild_id: int, user_id: int) -> Profile:
     profile.messages = await total_message_count(db, guild_id, user_id)
     profile.words = await get_word_count(msg_list, guild_id, user_id)
     profile.characters = await get_character_count(msg_list, guild_id, user_id)
-    profile.average_msg_length = profile.characters / profile.messages  # in chars
+    profile.average_msg_length = profile.characters / await get_notnull_message_count(db, guild_id, user_id)
 
     profile.total_embeds = await get_total_embeds(db, guild_id, user_id)
 
     profile.total_attachments = int(await get_total_attachments(db, guild_id, user_id))
     profile.top_words = await top_words(msg_list, guild_id, user_id)
 
-    # profile.top_emojis = await top_emoji(msg_list, guild_id, user_id)
+    profile.top_emojis = await top_emoji(msg_list, guild_id, user_id)
 
     # most_mentioned_ = await most_mentioned(db, guild_id, user_id)
     # profile.total_mentions = len(most_mentioned_)
